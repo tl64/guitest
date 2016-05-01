e79bee039be7e79d6a6221638a3e1463e1ebc1ca
Dragan89 <Archaic.dragan@gmail.com>
2016-05-02 01:16:22 +0200

    First Commit

---
+++ WpfGUITest/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfGUITest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //StackPanel mainWindow = new StackPanel();
+            //DockPanel mainWindow = new DockPanel();
+            //WrapPanel mainWindow = new WrapPanel();
+            //GridPanel mainWindow = new GridPanel();
+            //CanvasPanel mainWindow = new CanvasPanel();
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
